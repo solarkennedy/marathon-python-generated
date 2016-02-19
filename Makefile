8030760dc18a296180b31111a7ba04e8df47a4f6
Kyle Anderson <kwa@yelp.com>
2016-02-18 20:13:15 -0800

    convert to swagger first

--- Makefile
+++ Makefile
@@ -1,2 +1,17 @@
-prep:
-	npm install raml-python
+#marathon-python-generated: ./node_modules/.bin/raml2swagger marathon-code/.git
+#	./node_modules/.bin/raml2swagger marathon-code/docs/docs/rest-api/public/api/api.raml
+
+#node_modules/.bin/raml2swagger:
+#	npm install raml-to-swagger
+
+swagger.yaml:
+	node convert.js
+
+node_modules/api-spec-converter/:
+	npm install git+https://github.com/stoplightio/api-spec-converter.git
+
+marathon-code/.git:
+	git clone https://github.com/mesosphere/marathon.git marathon-code
+
+clean:
+	rm -rf node_modules marathon-code
---
+++ convert.js
@@ -0,0 +1,18 @@
+
+var slConverter = require('api-spec-converter'), //Warning: not published to npm yet
+    fs = require('fs')
+ramlToSwaggerConverter = new slConverter.Converter(slConverter.Formats.RAML, slConverter.Formats.SWAGGER)
+ramlToSwaggerConverter.loadFile('marathon-code/docs/docs/rest-api/public/api/api.raml', function(err){
+  if (err) {
+    console.log(err.stack);
+    return;
+  }
+
+  ramlToSwaggerConverter.convert('yaml')
+  .then(function(convertedData){
+    fs.writeFileSync('swagger.yaml', convertedData, 'utf8');
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+});
